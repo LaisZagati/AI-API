56047e8607fa77dc3bbe6606d4ec9003f5d7d733
LaisZagati <laisviana97@gmail.com>
2025-08-29 16:00:06 -0400

    AI API from Shecodes inserted

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+function showAnswer(response) {
+  console.log(response);
+  alert(response.data.answer);
+}
+
+let apiKey = "f093ocaff400a6043tff45112437b840";
+let context = "please provide a detailed answer";
+let prompt = "When was IA first created?";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Fetching data from API...");
+axios.get(apiUrl).then(showAnswer);
